2845d4f806adf09efa1eba6739d023058654bb2c
Hugo De Freitas <hugo.de-freitas@etu.univ-amu.fr>
2019-10-31 22:40:12 +0100

    update on json sources for DocViewerDocuments + vanilla templating with JS + server loaded resources with XMLHTTP requests

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,32 @@
-// function ready(callback){
-//     if (document.readyState!=='loading') callback();
-//     else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
-//     else document.attachEvent('onreadystatechange', function(){
-//             if (document.readyState==='complete') callback();
-//         });
-// }
-//
-// ready(function(){
-//     console.log('Document ready.');
-//     let docViewerLogo = document.querySelector('#docviewer-logo');
-//     UIkit.svg(docViewerLogo).svg.then(function(svg) {
-//         console.log();
-//         // svg.querySelector('path').style.stroke = 'red';
-//     });
-//
-//     let closeSidebar = document.querySelector('#close-sidebar');
-//     let openSidebar = document.querySelector('#open-sidebar');
-//     openSidebar.addEventListener('click', ()=>{
-//         UIkit.offcanvas('#sidebar').toggle();
-//     });
-//     closeSidebar.addEventListener('click', ()=>{
-//         UIkit.offcanvas('#sidebar').hide();
-//     });
-//
-//
-//
-// });
+let changeDocumentTriggers = document.querySelector('.change-document-trigger');
+DocViewerDocument.load('RFC2549').then((newDocument)=>{
+    let sideBar = document.querySelector('#sidebar');
+    sideBar.parentNode.insertBefore(newDocument.getElement(), sideBar.nextSibling);
+});
+
+
+/**================
+ * Fonction helpers pour le bon fonctionnement du site
+ =================*/
+/**
+ * Permet de charger un fichier via une XMLHTTP Request et une Promise
+ * @param path
+ */
+function loadFileOnServer(path)
+{
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function()
+        {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject("Erreur lors de la récupération sur le serveur.");
+                }
+            }
+        };
+        xhr.open("GET", path, true);
+        xhr.send();
+    });
+}
